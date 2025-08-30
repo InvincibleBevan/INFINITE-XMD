471e451ef64271c0cab3c4e28295934cc262bdf9
BEVANX <juliasiele15@gmail.com>
2025-08-30 16:20:38 +0300

    Update server.js

--- bevan/server.js
+++ bevan/server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Use absolute path for static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
